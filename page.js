227dd6cbf6e8137a10ae8c6c75aee8cc4a8b3b5b
genio-ai <hello@genio.systems>
2025-09-07 14:25:30 +0200

    Create page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Welcome to Genio OS</h1>
+      <p>Your Next.js + Tailwind app is running!</p>
+    </div>
+  );
+}
