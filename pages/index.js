21f46fda0d96ee30e70150dd9c40df65b7319cc4
genio-ai <hello@genio.systems>
2025-09-09 12:33:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 export default function Home() {
   return (
-    <div style={{padding:24,fontFamily:'system-ui'}}>
-      <header style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-        <img src="/genio-logo.svg" alt="Genio Logo" style={{height:80}} />
-        <img src="/genio-mascot.svg" alt="Genio Mascot" style={{height:80}} />
-      </header>
-
-      <main style={{marginTop:60,textAlign:'center'}}>
-        <h1>Welcome to Genio OS</h1>
-        <p>Logo left + Mascot right.</p>
-        <p><a href="/test">Go to /test</a></p>
-      </main>
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Welcome to Genio OS</h1>
+      <p>Logo and Mascot will be added here.</p>
+      <a href="/test" style={{ color: "blue" }}>Go to /test</a>
     </div>
   );
 }
