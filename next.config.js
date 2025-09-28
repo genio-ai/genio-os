38c9ae9cbf0422defa6dfb7bfecb980813ab8a8a
genio-ai <hello@genio.systems>
2025-09-28 18:55:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-// next.config.js at the project root
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // Map "@" to the project root so imports like "@/components/..." work in Vercel
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/index",
+        permanent: false,
+      },
+    ];
   },
 };
 
