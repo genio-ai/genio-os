6944c8a3b162d6e6c05bccff41a97417a14e6f4a
genio-ai <hello@genio.systems>
2025-10-07 08:02:50 +0200

    Create page.js

---
+++ app/twin/create/page.js
@@ -0,0 +1,8 @@
+// File: app/twin/create/page.js
+"use client";
+
+import OnboardingPage from "@/components/twin/OnboardingPage";
+
+export default function TwinCreatePage() {
+  return <OnboardingPage />;
+}
