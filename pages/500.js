b8bdc60304ea70e09e7490a46d8678e6cef3a926
genio-ai <hello@genio.systems>
2025-11-09 15:52:20 +0400

    Delete pages/500.js

--- pages/500.js
+++
@@ -1,9 +0,0 @@
-export default function ServerError() {
-  return (
-    <main className="page container">
-      <h1 className="h1">500</h1>
-      <p className="muted">Something went wrong.</p>
-      <a className="btn btn-primary" href="/">Go home</a>
-    </main>
-  );
-}
